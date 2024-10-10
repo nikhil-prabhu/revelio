4a08f23e00141c362407f0bf15a9298dc96d452f
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-10-10 17:05:54 +0530

    fix: make custom error compatible with Tauri return type

--- src-tauri/src/types.rs
+++ src-tauri/src/types.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 
-use thiserror::Error as ThisError;
+use serde::{Serialize, Serializer};
 
 /// Custom error type for errors encountered while gathering system information.
 #[non_exhaustive]
-#[derive(Debug, ThisError)]
+#[derive(Debug, thiserror::Error)]
 pub enum PeekError {
     #[error("An error occurred while retrieving CPU information: {0}")]
     CPUInfoError(Box<str>),
@@ -36,4 +36,53 @@ pub enum PeekError {
         #[source]
         Box<dyn Error + Send + Sync>,
     ),
+}
+
+#[derive(Debug, Serialize)]
+#[serde(tag = "kind", content = "message")]
+#[serde(rename_all = "camelCase")]
+enum PeekErrorKind {
+    CPUInfoError(String),
+
+    #[cfg(target_os = "windows")]
+    DirectXInfoError(String),
+
+    #[cfg(target_os = "macos")]
+    MetalInfoError(String),
+
+    #[cfg(target_os = "linux")]
+    VulkanInfoError(String),
+
+    #[cfg(target_os = "linux")]
+    OpenGLInfoError(String),
+
+    Error(String),
+}
+
+impl Serialize for PeekError {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let err_msg = self.to_string();
+        let err_kind = match self {
+            Self::CPUInfoError(_) => PeekErrorKind::CPUInfoError(err_msg),
+
+            #[cfg(target_os = "windows")]
+            Self::DirectXInfoError(_) => PeekErrorKind::DirectXInfoError(err_msg),
+
+            #[cfg(target_os = "macos")]
+            Self::MetalInfoError(_) => PeekErrorKind::MetalInfoError(err_msg),
+
+            #[cfg(target_os = "linux")]
+            Self::VulkanInfoError(_) => PeekErrorKind::VulkanInfoError(err_msg),
+
+            #[cfg(target_os = "linux")]
+            Self::OpenGLInfoError(_) => PeekErrorKind::OpenGLInfoError(err_msg),
+
+            Self::Error(_) => PeekErrorKind::Error(err_msg),
+        };
+
+        err_kind.serialize(serializer)
+    }
 }
