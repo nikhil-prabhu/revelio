746bcf0cf42837bdce522b3e171eb908ee2e9fd3
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-10-12 00:09:21 +0530

    feat: add some Vulkan symbol lookup maps

--- src-tauri/src/types.rs
+++ src-tauri/src/types.rs
@@ -1,6 +1,46 @@
 use std::error::Error;
 
+use phf::phf_map;
 use serde::{Serialize, Serializer};
+
+#[doc = "<https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/VkPhysicalDeviceType.html>"]
+#[derive(Serialize)]
+pub enum VulkanDeviceType {
+    #[serde(rename = "VK_PHYSICAL_DEVICE_TYPE_OTHER")]
+    Other,
+    #[serde(rename = "VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU")]
+    IntegratedGpu,
+    #[serde(rename = "VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU")]
+    DiscreteGpu,
+    #[serde(rename = "VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU")]
+    VirtualGpu,
+    #[serde(rename = "VK_PHYSICAL_DEVICE_TYPE_CPU")]
+    Cpu,
+}
+
+#[doc = "<https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/VkMemoryHeapFlagBits.html>"]
+#[derive(Serialize)]
+pub enum VulkanMemoryHeapFlags {
+    #[serde(rename = "VK_MEMORY_HEAP_DEVICE_LOCAL_BIT")]
+    DeviceLocal,
+    #[serde(rename = "VK_MEMORY_HEAP_MULTI_INSTANCE_BIT")]
+    MultiInstance,
+}
+
+/// Lookup map for raw Vulkan device type constant.
+pub static VK_DEVICE_TYPE_MAP: phf::Map<u32, VulkanDeviceType> = phf_map! {
+    0_u32 => VulkanDeviceType::Other,
+    1_u32 => VulkanDeviceType::IntegratedGpu,
+    2_u32 => VulkanDeviceType::DiscreteGpu,
+    3_u32 => VulkanDeviceType::VirtualGpu,
+    4_u32 => VulkanDeviceType::Cpu,
+};
+
+/// Lookup map for raw Vulkan device memory heap flags constant.
+pub static VK_MEMORY_HEAP_FLAGS_MAP: phf::Map<u32, VulkanMemoryHeapFlags> = phf_map! {
+    0_u32 => VulkanMemoryHeapFlags::DeviceLocal,
+    1_u32 => VulkanMemoryHeapFlags::MultiInstance,
+};
 
 /// Custom error type for errors encountered while gathering system information.
 #[non_exhaustive]
