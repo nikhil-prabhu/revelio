f314e31630b3e45f94aafbefa65c44783f5f59d1
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-10-12 21:52:09 +0530

    feat: add navigation menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
-import {NextUIProvider} from "@nextui-org/react";
+import {NextUIProvider, Tab, Tabs} from "@nextui-org/react";
+import {BsCpuFill, BsGpuCard, BsHddNetworkFill} from "react-icons/bs";
+import {RiHardDriveFill} from "react-icons/ri";
+import {GrSystem} from "react-icons/gr";
 
 function App() {
     return (
         <NextUIProvider>
-            <main className="light text-foreground bg-background h-screen flex flex-wrap flex-col items-center p-2">
-                <h1>Hello world</h1>
+            <main className="light text-foreground bg-background h-screen flex p-2">
+                <Tabs isVertical color="primary" className="place-self-center">
+                    <Tab key="cpu"
+                         title={
+                             <div className="flex items-center space-x-2">
+                                 <BsCpuFill/>
+                                 <span>CPU</span>
+                             </div>
+                         }
+                    ></Tab>
+
+                    <Tab key="gpu"
+                         title={
+                             <div className="flex items-center space-x-2">
+                                 <BsGpuCard/>
+                                 <span>GPU</span>
+                             </div>
+                         }></Tab>
+
+                    <Tab key="storage"
+                         title={
+                             <div className="flex items-center space-x-2">
+                                 <RiHardDriveFill/>
+                                 <span>Storage</span>
+                             </div>
+                         }></Tab>
+
+                    <Tab key="network"
+                         title={
+                             <div className="flex items-center space-x-2">
+                                 <BsHddNetworkFill/>
+                                 <span>Network</span>
+                             </div>
+                         }></Tab>
+
+                    <Tab key="Platform"
+                         title={
+                             <div className="flex items-center space-x-2">
+                                 <GrSystem/>
+                                 <span>Platform</span>
+                             </div>
+                         }></Tab>
+                </Tabs>
             </main>
         </NextUIProvider>
     );
