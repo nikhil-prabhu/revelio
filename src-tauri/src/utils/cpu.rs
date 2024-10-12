83ae17e2220542b3f1ea97fb83488208ff3994b5
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-10-12 16:29:12 +0530

    refactor!: better separation of logical processors

--- src-tauri/src/utils/cpu.rs
+++ src-tauri/src/utils/cpu.rs
@@ -1,6 +1,15 @@
 use serde::Serialize;
 use sysinfo::{CpuRefreshKind, RefreshKind, System};
 
+/// Contains information of a single logical processor.
+#[derive(Debug, Serialize)]
+pub struct Cpu {
+    /// The CPU index.
+    pub idx: usize,
+    /// The CPU frequency.
+    pub frequency: u64,
+}
+
 // TODO: include lower level information (Eg: cache, op-mode(s), etc.).
 /// Contains information of the system's CPU/Processor.
 #[derive(Debug, Serialize)]
@@ -13,10 +22,10 @@ pub struct CpuInfo {
     pub vendor_id: String,
     /// The CPU brand.
     pub brand: String,
-    /// The frequency of the CPU.
-    pub frequency: u64,
     /// The total number of physical cores.
     pub physical_core_count: Option<usize>,
+    /// The logical processors on the CPU.
+    pub cpus: Vec<Cpu>,
 }
 
 impl CpuInfo {
@@ -28,14 +37,22 @@ impl CpuInfo {
         let physical_core_count = sys.physical_core_count();
         let cpus = sys.cpus();
         let cpu = &cpus[0];
+        let mut logical_proc = Vec::with_capacity(physical_core_count.unwrap_or(8));
+
+        for (idx, c) in cpus.iter().enumerate() {
+            logical_proc.push(Cpu {
+                idx,
+                frequency: c.frequency(),
+            });
+        }
 
         Self {
             arch: System::cpu_arch(),
             name: cpu.name().into(),
             vendor_id: cpu.vendor_id().into(),
             brand: cpu.brand().into(),
-            frequency: cpu.frequency(),
             physical_core_count,
+            cpus: logical_proc,
         }
     }
 }
