26398602415407a709412b5ec058dc96bced4441
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-10-18 16:07:03 +0530

    feat: set window title based on current view

--- src/components/ViewContainer.tsx
+++ src/components/ViewContainer.tsx
@@ -1,4 +1,5 @@
-import {ReactNode} from "react";
+import {ReactNode, useEffect} from "react";
+import {getCurrentWindow} from "@tauri-apps/api/window";
 
 export interface ViewContainerProps {
     title: string;
@@ -6,6 +7,16 @@ export interface ViewContainerProps {
 }
 
 function ViewContainer(props: ViewContainerProps) {
+    useEffect(() => {
+        let window = getCurrentWindow();
+
+        window.setTitle(`Revelio - ${props.title}`).then(() => {
+            console.info("Set window title successfully");
+        }).catch(error => {
+            console.error(error);
+        });
+    }, []);
+
     return (
         <div className={"w-full flex-row text-center items-center mb-4 wl-2 mr-2"}>
             <h1 className="font-bold m-4">{props.title}</h1>
