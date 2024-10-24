e22ec37c33ea299b239c3849b081a607d4e37816
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-10-24 21:30:19 +0530

    refactor: move Vulkan info to separate component

---
+++ src/components/VulkanInfo.tsx
@@ -0,0 +1,190 @@
+import {
+    Card,
+    CardBody,
+    Divider,
+    Image,
+    Select, Selection,
+    SelectItem,
+    Spacer, Spinner,
+    Tab,
+    Table, TableBody, TableCell, TableColumn,
+    TableHeader, TableRow,
+    Tabs
+} from "@nextui-org/react";
+import * as utils from "../utils.ts";
+import {VulkanInfo as VkInfo, commands} from "../bindings";
+import {useEffect, useState} from "react";
+import {useTheme} from "next-themes";
+
+interface DeviceProps {
+    info: VkInfo;
+    currentDevice: number;
+}
+
+function Device(props: DeviceProps) {
+    const {info, currentDevice} = props;
+
+    return (
+        <Card shadow="none">
+            <CardBody>
+                {info.devices.map(device => (
+                    device.index === currentDevice ?
+                        (<Table isStriped shadow="none" fullWidth>
+                            <TableHeader>
+                                <TableColumn>Property</TableColumn>
+                                <TableColumn>Value</TableColumn>
+                            </TableHeader>
+
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell className="font-bold w-1/3">Device
+                                        Name</TableCell>
+                                    <TableCell
+                                        className="font-mono">{device.deviceName}</TableCell>
+                                </TableRow>
+
+                                <TableRow>
+                                    <TableCell className="font-bold w-1/3">Vendor
+                                        ID</TableCell>
+                                    <TableCell
+                                        className="font-mono">{device.vendorId}</TableCell>
+                                </TableRow>
+
+                                <TableRow>
+                                    <TableCell className="font-bold w-1/3">Device
+                                        ID</TableCell>
+                                    <TableCell
+                                        className="font-mono">{device.deviceId}</TableCell>
+                                </TableRow>
+
+                                <TableRow>
+                                    <TableCell className="font-bold w-1/3">Device
+                                        Type</TableCell>
+                                    <TableCell
+                                        className="font-mono">{device.deviceType}</TableCell>
+                                </TableRow>
+
+                                <TableRow>
+                                    <TableCell className="font-bold w-1/3">API
+                                        Version</TableCell>
+                                    <TableCell
+                                        className="font-mono">{device.apiVersion}</TableCell>
+                                </TableRow>
+
+                                <TableRow>
+                                    <TableCell className="font-bold w-1/3">Driver
+                                        Version</TableCell>
+                                    <TableCell
+                                        className="font-mono">{device.driverVersion}</TableCell>
+                                </TableRow>
+
+                                <TableRow>
+                                    <TableCell className="font-bold w-1/3">Pipeline Cache
+                                        UUID</TableCell>
+                                    <TableCell
+                                        className="font-mono">{device.pipelineCacheUuid}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>)
+                        : null
+                ))}
+            </CardBody>
+        </Card>
+    );
+}
+
+function VulkanInfo() {
+    const [vulkanInfo, setVulkanInfo] = useState<VkInfo>();
+    const [currentDevice, setCurrentDevice] = useState<Selection>(new Set(["0"]));
+    const [currentTheme, setCurrentTheme] = useState<utils.Variant>("light");
+    const {theme} = useTheme();
+
+    useEffect(() => {
+        if (theme!.toLowerCase().includes("dark")) {
+            setCurrentTheme("dark");
+        } else {
+            setCurrentTheme("light");
+        }
+
+        commands.getVulkanInfo().then(info => {
+            setVulkanInfo(info);
+            console.debug(info);
+        }).catch(error => {
+            console.error(error);
+        });
+    }, [theme]);
+
+    if (!vulkanInfo) {
+        return <Spinner label="Loading..." color="primary"/>
+    }
+
+    function getCurrentDeviceName(): string {
+        for (const device of vulkanInfo!.devices) {
+            if (Number([...currentDevice][0]) === device.index) {
+                return device.deviceName;
+            }
+        }
+
+        return "unknown"
+    }
+
+    return (
+        <>
+            <div className="flex items-center justify-center space-x-8">
+                <Select
+                    autoFocus
+                    items={vulkanInfo.devices}
+                    label="Select Device"
+                    selectionMode="single"
+                    disallowEmptySelection
+                    onSelectionChange={setCurrentDevice}
+                    selectedKeys={currentDevice}
+                    variant="faded"
+                    startContent={
+                        <Image src={utils.getVendorIcon(getCurrentDeviceName(), currentTheme)}
+                               width={16}
+                               height={16}
+                               radius="none"/>
+                    }
+                    className="max-w-md"
+                >
+                    {(device) => <SelectItem
+                        key={device.index}
+                        startContent={(
+                            <Image src={utils.getVendorIcon(device.deviceName, currentTheme)} width={16}
+                                   height={16}
+                                   radius="none"/>
+                        )}
+                    >{`${device.index}: ${device.deviceName}`}</SelectItem>}
+                </Select>
+
+                <Image src={utils.getGpuIcon(getCurrentDeviceName(), currentTheme)} removeWrapper
+                       width={100}
+                       height={100}
+                       radius="none"
+                />
+            </div>
+
+            <Spacer y={4}/>
+            <Divider/>
+
+            <Tabs
+                placement="end"
+                color="primary"
+                disabledKeys={["limits", "extensions", "layers"]}
+                variant="underlined"
+                className="m-4">
+
+                <Tab key="device" title="Device" className="w-full">
+                    <Device currentDevice={Number([...currentDevice][0])} info={vulkanInfo}/>
+                </Tab>
+
+                <Tab key="limits" title="Limits"/>
+                <Tab key="extensions" title="Extensions"/>
+                <Tab key="layers" title="Layers"/>
+            </Tabs>
+        </>
+    );
+}
+
+export default VulkanInfo;
--- src/views/Gpu.tsx
+++ src/views/Gpu.tsx
@@ -1,24 +1,15 @@
 import {useEffect, useState} from "react";
-import {VulkanInfo, commands} from "../bindings";
 import * as utils from "../utils";
 import {
     Card,
     CardBody,
-    Select, SelectItem, Selection, Spacer,
-    Spinner,
-    Tab,
-    Table,
-    TableBody, TableCell,
-    TableColumn,
-    TableHeader, TableRow,
-    Tabs, Image, Divider
+    Tab, Tabs, Image,
 } from "@nextui-org/react";
 import ViewContainer from "../components/ViewContainer";
 import {useTheme} from "next-themes";
+import VulkanInfo from "../components/VulkanInfo.tsx";
 
 function Gpu() {
-    const [vulkanInfo, setVulkanInfo] = useState<VulkanInfo>();
-    const [currentDevice, setCurrentDevice] = useState<Selection>(new Set(["0"]));
     const [currentTheme, setCurrentTheme] = useState<utils.Variant>("light");
     const {theme} = useTheme();
 
@@ -28,33 +19,8 @@ function Gpu() {
         } else {
             setCurrentTheme("light");
         }
-
-        commands.getVulkanInfo().then(info => {
-            setVulkanInfo(info);
-            console.info("Vulkan information retrieved successfully.");
-        }).catch(error => {
-            console.error(error);
-        });
     }, [theme]);
 
-    // TODO: improve loading indicator.
-    if (!vulkanInfo) {
-        return <Spinner label="Loading..." color="primary"/>
-    }
-
-    function getCurrentDeviceName(): string {
-        for (const device of vulkanInfo!.devices) {
-            if (Number([...currentDevice][0]) === device.index) {
-                return device.deviceName;
-            }
-        }
-
-        return "unknown"
-    }
-
-    // TODO: add brand and vendor icons.
-    // TODO: add advanced vulkan info (layers, limits, etc.).
-    // TODO: move Vulkan, OpenGL, etc. to separate components.
     return (
         <ViewContainer title="GPU Information">
             <Card shadow="sm">
@@ -68,122 +34,7 @@ function Gpu() {
                             }
                             titleValue="Vulkan"
                         >
-                            <div className="flex items-center justify-center space-x-8">
-                                <Select
-                                    autoFocus
-                                    items={vulkanInfo.devices}
-                                    label="Select Device"
-                                    selectionMode="single"
-                                    disallowEmptySelection
-                                    onSelectionChange={setCurrentDevice}
-                                    selectedKeys={currentDevice}
-                                    variant="faded"
-                                    startContent={
-                                        <Image src={utils.getVendorIcon(getCurrentDeviceName(), currentTheme)}
-                                               width={16}
-                                               height={16}
-                                               radius="none"/>
-                                    }
-                                    className="max-w-md"
-                                >
-                                    {(device) => <SelectItem
-                                        key={device.index}
-                                        startContent={(
-                                            <Image src={utils.getVendorIcon(device.deviceName, currentTheme)} width={16}
-                                                   height={16}
-                                                   radius="none"/>
-                                        )}
-                                    >{`${device.index}: ${device.deviceName}`}</SelectItem>}
-                                </Select>
-
-                                <Image src={utils.getGpuIcon(getCurrentDeviceName(), currentTheme)} removeWrapper
-                                       width={100}
-                                       height={100}
-                                       radius="none"
-                                />
-                            </div>
-
-                            <Spacer y={4}/>
-                            <Divider/>
-
-                            <Tabs
-                                placement="end"
-                                color="primary"
-                                disabledKeys={["limits", "extensions", "layers"]}
-                                variant="underlined"
-                                className="m-4">
-                                <Tab key="device" title="Device" className="w-full">
-                                    <Card shadow="none">
-                                        <CardBody>
-                                            {vulkanInfo.devices.map(device => (
-                                                device.index === Number([...currentDevice][0]) ?
-                                                    (<Table isStriped shadow="none" fullWidth>
-                                                        <TableHeader>
-                                                            <TableColumn>Property</TableColumn>
-                                                            <TableColumn>Value</TableColumn>
-                                                        </TableHeader>
-
-                                                        <TableBody>
-                                                            <TableRow>
-                                                                <TableCell className="font-bold w-1/3">Device
-                                                                    Name</TableCell>
-                                                                <TableCell
-                                                                    className="font-mono">{device.deviceName}</TableCell>
-                                                            </TableRow>
-
-                                                            <TableRow>
-                                                                <TableCell className="font-bold w-1/3">Vendor
-                                                                    ID</TableCell>
-                                                                <TableCell
-                                                                    className="font-mono">{device.vendorId}</TableCell>
-                                                            </TableRow>
-
-                                                            <TableRow>
-                                                                <TableCell className="font-bold w-1/3">Device
-                                                                    ID</TableCell>
-                                                                <TableCell
-                                                                    className="font-mono">{device.deviceId}</TableCell>
-                                                            </TableRow>
-
-                                                            <TableRow>
-                                                                <TableCell className="font-bold w-1/3">Device
-                                                                    Type</TableCell>
-                                                                <TableCell
-                                                                    className="font-mono">{device.deviceType}</TableCell>
-                                                            </TableRow>
-
-                                                            <TableRow>
-                                                                <TableCell className="font-bold w-1/3">API
-                                                                    Version</TableCell>
-                                                                <TableCell
-                                                                    className="font-mono">{device.apiVersion}</TableCell>
-                                                            </TableRow>
-
-                                                            <TableRow>
-                                                                <TableCell className="font-bold w-1/3">Driver
-                                                                    Version</TableCell>
-                                                                <TableCell
-                                                                    className="font-mono">{device.driverVersion}</TableCell>
-                                                            </TableRow>
-
-                                                            <TableRow>
-                                                                <TableCell className="font-bold w-1/3">Pipeline Cache
-                                                                    UUID</TableCell>
-                                                                <TableCell
-                                                                    className="font-mono">{device.pipelineCacheUuid}</TableCell>
-                                                            </TableRow>
-                                                        </TableBody>
-                                                    </Table>)
-                                                    : null
-                                            ))}
-                                        </CardBody>
-                                    </Card>
-                                </Tab>
-
-                                <Tab key="limits" title="Limits"/>
-                                <Tab key="extensions" title="Extensions"/>
-                                <Tab key="layers" title="Layers"/>
-                            </Tabs>
+                            <VulkanInfo/>
                         </Tab>
 
                         <Tab
