fcfd593ef182b961199fd7c405caef4fe8868c67
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-10-26 13:14:37 +0530

    feat(ui): switch window theme when switching app theme

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@ import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { Switch } from "@nextui-org/react";
 import { FaMoon, FaSun } from "react-icons/fa";
+import { getCurrentWindow, Theme } from "@tauri-apps/api/window";
 
 function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
@@ -15,11 +16,24 @@ function ThemeSwitcher() {
     return null;
   }
 
+  async function setWindowTheme(theme: string) {
+    getCurrentWindow()
+      .setTheme(theme as Theme)
+      .then(() => {
+        console.debug(`Successfully set window theme to ${theme}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   function handleThemeSwitch(isSelected: boolean) {
     if (isSelected) {
       setTheme("dark");
+      setWindowTheme("dark").then();
     } else {
       setTheme("light");
+      setWindowTheme("light").then();
     }
   }
 
